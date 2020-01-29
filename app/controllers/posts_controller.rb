3ac6664019f1be6dcb9423c4e31d3f6cef9a353f
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-29 05:36:48 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
 	def index
 		@posts = Post.all
+		render "posts/index"
 	end
 
 	def show
 		@post = Post.find(params[:id])
+		render "posts/show"
 	end
 end
